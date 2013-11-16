937a8288e1e84c75b2192c5d0497e415ecab8a1a
Feifan Wang <feifanw@gmail.com>
2013-11-15 18:44:00 -0800

    changed "collection" to proper todo-list terms.

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ var app = express();
 app.use(express.bodyParser());
 var db = mongoskin.db('localhost:27017/test', {safe:true});
 
-app.param('collectionName', function(req, res, next, collectionName){
-  req.collection = db.collection(collectionName);
+app.param('listName', function(req, res, next, listName){
+  req.collection = db.collection(listName);
   return next();
 });
 
 app.get('/', function(req, res){
-  res.send('please select a collection, e.g., /collections/messages');
+  res.send('please select a collection, e.g., /todos/messages');
 });
 
-app.get('/collections/:collectionName', function(req, res){
+app.get('/todos/:listName', function(req, res){
   req.collection.find({}, {limit: 10, sort: [['_id', -1]]}).toArray(function(err, results){
     if(err) return next(err);
     res.send(results);
