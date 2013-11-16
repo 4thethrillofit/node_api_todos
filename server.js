b0eaaaa9f7744364a9ea68e41f353a890ad8d9a6
Feifan Wang <feifanw@gmail.com>
2013-11-15 19:56:00 -0800

    PUT/update endpoint.

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ app.param('listName', function(req, res, next, listName){
   return next();
 });
 
+// sending a message to user if the user does not request a specific resource
 app.get('/', function(req, res){
   res.send('please select a collection, e.g., /todos/messages');
 });
 
+// GET a specific todoList collection
 app.get('/todos/:listName', function(req, res){
   req.collection.find({}, {limit: 10, sort: [['_id', -1]]}).toArray(function(err, results){
     if(err) return next(err);
@@ -20,6 +22,7 @@ app.get('/todos/:listName', function(req, res){
   });
 });
 
+// GET a todo item
 app.get('/todos/:listName/:id', function(req, res){
   req.collection.findOne({_id: req.collection.id(req.params.id)}, function(err, result){
     if(err) return next(err);
@@ -27,6 +30,7 @@ app.get('/todos/:listName/:id', function(req, res){
   });
 });
 
+// POST a new todo item
 app.post('/todos/:listName', function(req, res){
   req.collection.insert(req.body, {}, function(err, results){
     if(err) return next(err);
@@ -34,5 +38,20 @@ app.post('/todos/:listName', function(req, res){
   });
 });
 
+// PUT existing todo item
+// in Mongo, PUT returns a count of affected objects
+// {$set:req.body} is special Mongo operator that sets values
+// {safe:true, multi:false} tells Mongo to wait for execution b4 running callback and
+// to process only one item.
+app.put('/todos/:listName/:id', function(req, res){
+  req.collection.update({_id: req.collection.id(req.params.id)}, {$set:req.body}, {safe:true, multi:false}, function(err, result){
+    if(err) return next(err);
+    res.send(
+      result === 1 ? {msg: 'success'} : {msg: 'error:' + err}
+    );
+  });
+});
+
+// Start the Node server
 var server = app.listen(3000);
 console.log("Express server listening on port %d in %s mode", server.address().port, app.settings.env);
