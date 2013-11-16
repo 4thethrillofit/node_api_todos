04405647836975f4e433ca2eddc2c790665517b3
Feifan Wang <feifanw@gmail.com>
2013-11-15 16:30:21 -0800

    additional modules - express and mongoskin

--- express.test.js
+++ express.test.js
@@ -2,6 +2,7 @@ var superagent = require('superagent');
 var expect = require('chai').expect;
 
 describe('express rest api server', function(){
+  var id;
   it('posts a todo item', function(done){
     superagent.post('http://localhost:3000/todos/test')
       .send({
@@ -13,6 +14,8 @@ describe('express rest api server', function(){
         console.log(err);
         expect(err).to.equal(null);
         expect(res.body.length).to.equal(1);
+        expect(res.body[0]._id.length).to.equal(24);
+        id = res.body[0]._id;
         done();
       });
   });
