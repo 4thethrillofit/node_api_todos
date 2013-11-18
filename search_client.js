3e60da769132bb8b1e7813d972fa7cc462bfe6d5
Feifan Wang <feifanw@gmail.com>
2013-11-17 18:50:32 -0800

    function for constructing queryObj abstracted from the server.

--- search_client.js
+++ search_client.js
@@ -1,6 +1,7 @@
 var ElasticSearchClient = require('elasticsearchclient');
 var keys = require('./keys');
 
+var titleBoostValue = 2;
 var serverOptions = {
   host: 'api.searchbox.io',
   port: 80,
@@ -12,5 +13,17 @@ var serverOptions = {
 };
 
 var elasticSearchClient = new ElasticSearchClient(serverOptions);
+
+elasticSearchClient.buildQueryObj = function(query) {
+  var queryObj = {
+    'query': {
+      "multi_match" : {
+        "query" : query,
+        "fields" : [ "title^"+titleBoostValue, "body"  ]
+      }
+    }
+  }
+  return queryObj;
+};
 
 module.exports.SearchClient = elasticSearchClient;
